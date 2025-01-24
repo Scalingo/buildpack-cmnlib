34d68af7e7ddb7e7f064aede87bbb83624f1e310
François KUBLER <francois+github@kubler.org>
2025-01-24 16:32:53 +0100

    feat: add comments and new functions

--- cmnlib.sh
+++ cmnlib.sh
@@ -1,68 +1,92 @@
 #!/usr/bin/env bash
 
-STEP="------>"
-RED="\033[1;31m"
-GREEN="\033[1;32m"
-YELLOW="\033[1;33m"
-NC="\033[0m"
-
-readonly RED
-readonly GREEN
-readonly YELLOW
-readonly NC
-
-
-# `cmn::output::debug`, `cmn::output::info`, `cmn::output::warn` and
-# `cmn::output::err` leverage the same trick:
-# Calling `exec` without a /command/ argument (which is the case here) applies
-# any redirection applied to it to the current shell.
-# Consequently, calling `exec <<< "${@}" feeds stdin with $@.
-# This allows the function to be called with an argument or with an heredoc.
-
-
 cmn::output::info() {
+#
+# Outputs an informational message on stdout.
+# Can be called with a string argument or with a Bash heredoc.
+#
+
+	# Calling `exec` without a /command/ argument (which is the case here)
+	# applies any redirection applied to it to the current shell.
+	# Consequently, calling `exec <<< "${@}" feeds stdin with $@.
+	# This allows the function to be called with an argument or with an
+	# heredoc.
 	[[ ${#} -gt 0 ]] && exec <<< "${@}"
 
+	# Read `stdin` line by line and outputs each line formatted:
 	while read -r line; do
-		printf "\t%b\n" "${line}"
+		printf "     %b\n" "${line}"
 	done
 }
 
 cmn::output::warn() {
+#
+# Outputs a warning message on stdout.
+# Can be called with a string argument or with a Bash heredoc.
+#
+
+	# Calling `exec` without a /command/ argument (which is the case here)
+	# applies any redirection applied to it to the current shell.
+	# Consequently, calling `exec <<< "${@}" feeds stdin with $@.
+	# This allows the function to be called with an argument or with an
+	# heredoc.
 	[[ ${#} -gt 0 ]] && exec <<< "${@}"
 
+	# Read `stdin` line by line and outputs each line formatted:
 	while read -r line; do
-		printf "%b !\t%b%b\n" "${YELLOW}" "${line}" "${NC}"
+		printf " !   %b\n" "${line}"
 	done
 }
 
 cmn::output::err() {
+#
+# Outputs an error message on stderr.
+# Can be called with a string argument or with a Bash heredoc.
+#
+# Globals:
+# - RED
+# - NC
+#
+
+	# Calling `exec` without a /command/ argument (which is the case here)
+	# applies any redirection applied to it to the current shell.
+	# Consequently, calling `exec <<< "${@}" feeds stdin with $@.
+	# This allows the function to be called with an argument or with an
+	# heredoc.
 	[[ ${#} -gt 0 ]] && exec <<< "${@}"
 
+	# Read `stdin` line by line and outputs each line formatted:
 	while read -r line; do
-		printf "%b !!\t%b%b\n" "${RED}" "${line}" "${NC}" >&2
+		printf " !!  %b\n" "${line}" >&2
 	done
 
-	if [[ -n ${BUILDPACK_DEBUG} ]]; then
-		printf " !!\t%s\n" "Traceback:"
-
-		for (( i=1; i<${#FUNCNAME[@]}; i++ )); do
-			>&2 printf " !!\t%s: %s: %s\n" \
-				"${BASH_SOURCE[i]}" \
-				"${FUNCNAME[$i]}" \
-				"${BASH_LINENO[$i-1]}"
-		done
+	if [[ -n "${BUILDPACK_DEBUG}" ]]; then
+		cmn::output::traceback
 	fi
 }
 
 cmn::output::debug() {
+#
+# Outputs a debug message on stdout.
+# Can be called with a string argument or with a Bash heredoc.
+# Only outputs when BUILPACK_DEBUG is set!
+#
+
+	# Return ASAP if BUILDPACK_DEBUG isn't set
 	[[ -z "${BUILDPACK_DEBUG}" ]] && return
 
+	# Calling `exec` without a /command/ argument (which is the case here)
+	# applies any redirection applied to it to the current shell.
+	# Consequently, calling `exec <<< "${@}" feeds stdin with $@.
+	# This allows the function to be called with an argument or with an
+	# heredoc.
 	[[ ${#} -gt 0 ]] && exec <<< "${@}"
 
 	echo
+	# Read `stdin` line by line and outputs each line formatted.
+	# We also add some traceback information (filename, function and lineno)
 	while read -r line; do
-		printf " *\t%s: %s: %s: %s\n" \
+		printf " *   %s: %s: %s: %s\n" \
 			"${BASH_SOURCE[1]}" \
 			"${FUNCNAME[1]}" \
 			"${BASH_LINENO[0]}" \
@@ -70,76 +94,234 @@ cmn::output::debug() {
 	done
 }
 
+cmn::output::traceback() {
+#
+# Outputs a traceback to stderr.
+#
+
+	printf " !!  Traceback:\n" >&2
+
+	for (( i=1; i<${#FUNCNAME[@]}; i++ )); do
+		>&2 printf " !!    %s: %s: %s\n" \
+			"${BASH_SOURCE[i]}" \
+			"${FUNCNAME[$i]}" \
+			"${BASH_LINENO[$i-1]}"
+	done
+}
+
 
 
 cmn::trap::setup() {
+#
+# Instructs the buildpack to catch the `EXIT`, `SIGHUP`, `SIGINT`,
+# `SIGQUIT`, `SIGABRT`, and `SIGTERM` signals and to call `cmn::main::fail`
+# when it happens.
+#
+
 	trap cmn::main::fail EXIT SIGHUP SIGINT SIGQUIT SIGABRT SIGTERM
 }
 
 cmn::trap::teardown() {
+#
+# Instructs the buildpack to stop catching the `EXIT`, `SIGHUP`, `SIGINT`,
+# `SIGQUIT`, `SIGABRT`, and `SIGTERM` signals.
+#
+
 	trap - EXIT SIGHUP SIGINT SIGQUIT SIGABRT SIGTERM
 }
 
 
 
 cmn::main::start() {
+#
+# Marks the begining of the buildpack.
+# Calls `cmn::trap::setup`.
+# Use this function at the beginning of the buildpack.
+#
+
 	cmn::trap::setup
 }
 
 cmn::main::finish() {
+#
+# Outputs a success message and exits with a `0` return code, thus
+# instructing the platform that the buildpack ran successfully.
+# Calls `cmn::trap::teardown`.
+# Use this function as the last instruction of the buildpack, when it
+# succeeded.
+#
+
 	cmn::trap::teardown
-	printf "\n%b%b%b\n" "${GREEN}" "All done." "${NC}"
+	printf "\n%b\n" "All done."
 	exit 0
 }
 
 cmn::main::fail() {
+#
+# Outputs an error message and exits with a `1` return code, thus
+# instructing the platform that the buildpack failed (and so did the
+# build).
+# Calls `cmn::trap::teardown`.
+# Use this function as the last instruction of the buildpack, when it
+# failed.
+#
+
 	cmn::trap::teardown
-	printf "\n%b%b%b\n" "${RED}" "Failed." "${NC}" >&2
+	printf "\n%b\n" "Failed." >&2
 	exit 1
 }
 
 
 
 cmn::step::start() {
-	printf "%s\t%b\n" "${STEP}" "${*}"
+#
+# Outputs a message marking the beginning of a buildpack step. A step is a
+# group of tasks that are logically bound.
+# Use this function when the step is about to start.
+#
+
+	printf "%s %b\n" "--->" "${*}"
 }
 
 cmn::step::finish() {
-	printf "%b\t%b%b\n" "${GREEN}" "Done." "${NC}"
+#
+# Outputs a success message marking the end of a buildpack step.
+# Use this function when the step succeeded.
+#
+
+	printf "     %b\n" "Done."
 }
 
 cmn::step::fail() {
-	printf "%b\t%b%b\n" "${RED}" "Failed." "${NC}"
+#
+# Outputs an error message marking the end of a buildpack step.
+# Use this function when the step failed.
+#
+
+	printf " %b\n" "Failed."
 }
 
 
 
 cmn::task::start() {
-	echo -n "	$*... "
+#
+# Outputs a message marking the beginning of a buildpack task. A task is a
+# single instruction, such as downloading a file, extracting an archive,...
+# Use this function when the task is about to start.
+#
+
+	echo -n "     $*... "
 }
 
 cmn::task::finish() {
+#
+# Outputs a success message marking the end of a task.
+# Use this function when the task succeeded.
+#
+
 	echo "OK."
 }
 
 cmn::task::fail() {
+#
+# Outputs an error message marking the end of a task.
+# Calls `cmn::ouput::err` with `$1` when `$1` is set.
+#
+
 	echo "Failed."
 
-	if [[ -n "${1}" ]]; then
-		cmn::output::err "${1}"
-	fi
+	[[ -n "${1}" ]] && cmn::output::err "${1}"
+}
+
+
+
+cmn::cache::get() {
+#
+# Checks whether the given file exists in `CACHE_DIR`.
+# If so, returns 0, else 1.
+#
+
+	local rc
+	#local filepath
+
+	rc=1
+
+	printf -v filepath "%s/%s" "${CACHE_DIR}" "${1}"
+
+	cmn::output::debug "Checking whether '${filepath}' exists or not."
+
+	[[ -f "${filepath}" ]]
+}
+
+cmn::cache::put() {
+#
+# Copies the given file in `CACHE_DIR`.
+# Destination is relative to `CACHE_DIR`.
+#
+# If destination involves a path, makes sure the directories exist before
+# trying to copy the file(s).
+#
+# If destination is not set, the file is copied to `CACHE_DIR` with the same
+# name.
+#
+# Examples:
+#
+# cmn::cache::put "archive.tar.gz" "archive-version.tar.gz"
+# copies `archive.tar.gz` to `${CACHE_DIR}/archive-version.tar.gz`
+#
+# cmn::cache::put "archive.tar.gz" "lib/version/archive.tar.gz"
+# copies `archive.tar.gz` to `${CACHE_DIR}/lib/version/archive.tar.gz`
+#
+# cmn::cache::put "downloads/archive.tar.gz`
+# copies `archive.tar.gz` to `${CACHE_DIR}/archive.tar.gz`
+#
+# cmn::cache::put "my_directory"
+# copies `my_directory` (and its content) to `${CACHE_DIR}/my_directory`
+#
+
+	local rc
+
+	local src
+	local dst
+	local cp_output
+
+	rc=1
+	src="${1}"
+	# Defaults to basename($1) when $2 is not set:
+	dst="${CACHE_DIR}/${2:-"${src##*/}"}"
+
+	# Make sure target path exists:
+	mkdir --parents "$( dirname "${dst}" )"
+
+	cp_output="$( cp --force --recursive --verbose "${src}" "${dst}" 2>&1 )"
+	rc="${?}"
+
+	cmn::output::debug <<- EOM
+		Copying '${src}' to '${dst}':
+		${cp_output}
+	EOM
+
+	return "${rc}"
 }
 
 
 
 cmn::file::check_checksum() {
+#
+# Computes the checksum of a file and checks that it matches the one stored in
+# the reference file.
+# md5, sha1 and sha256 hashing algorithm are currently supported.
+#
+# $1: file
+# $2: checksum file
+#
+
 	local rc
 
 	local file
-
 	local hash_file
 	local hash_algo
-	local hash
+	local ref_hash
 
 	rc=1
 	file="${1}"; shift
@@ -147,21 +329,24 @@ cmn::file::check_checksum() {
 
 	hash_algo="${hash_file##*.}"
 
-	hash="$( cut -d " " -f 1 < "${hash_file}" )"
+	ref_hash="$( cut -d " " -f 1 < "${hash_file}" )"
 
 	case "${hash_algo}" in
 		"sha1")
-			shasum --algorithm 1 --check --status <<< "${hash}  ${file}"
+			shasum --algorithm 1 --check --status <<< "${ref_hash}  ${file}"
 			rc="${?}"
 			;;
+
 		"sha256")
-			shasum --algorithm 256 --check --status <<< "${hash}  ${file}"
+			shasum --algorithm 256 --check --status <<< "${ref_hash}  ${file}"
 			rc="${?}"
 			;;
+
 		"md5")
-		    md5sum --check --status <<< "${hash}  ${file}"
+		    md5sum --check --status <<< "${ref_hash}  ${file}"
 			rc="${?}"
 			;;
+
 		*)
 			rc=2
 			;;
@@ -171,17 +356,92 @@ cmn::file::check_checksum() {
 }
 
 cmn::file::download() {
-	local rc
+#
+# Downloads the file pointed by the given URL and stores it at the given path.
+#
+# $1: URL of the file to download
+# $2: (opt) Path where to output the downloaded file. Defaults to /dev/stdout.
+#
+
 	local url
-	local cached
+	local out
+
+	url="${1}"; shift
+	out="${1:-"-"}"; shift
+
+	if [[ -n "${BUILDPACK_DEBUG}" ]]; then
+		curl --silent --fail --retry 3 \
+			--location "${url}" --output "${out}"
+	else
+		curl --silent --fail --retry 3 \
+			--location "${url}" --output "${out}"
+	fi
+
+	return "${?}"
+}
+
+cmn::file::download_and_check() {
+#
+# Downloads a file from the specified URL, stores it at the specified path.
+# Also downloads the checksum from the specified URL, stores it at the
+# specified path.
+# Finally checks the hash of the downloaded file against the downloaded
+# checksum.
+#
+# Calls `cmn::file::download`
+# Calls `cmn::file::check_checksum`
+# Calls `cmn::jobs::wait`
+#
+# $1: file URL
+# $2: checksum URL
+# $3: file path (where to store the downloaded file)
+# $4: hash path (where to store the downloaded checksum file)
+#
+
+	local rc
+
+	local file_url
+	local hash_url
+	local file_path
+	local hash_path
 
 	rc=1
-	url="${1}"; shift
-	cached="${1}"; shift
+	file_url="${1}"; shift
+	hash_url="${1}"; shift
+	file_path="${1}"; shift
+	hash_path="${1}"; shift
 
-	if curl --silent --retry 3 --location "${url}" --output "${cached}"; then
-		rc=0
+	cmn::file::download "${file_url}" "${file_path}" &
+	cmn::file::download "${hash_url}" "${hash_path}" &
+
+	if cmn::jobs::wait; then
+		cmn::file::check_checksum "${file_path}" "${hash_path}"
+		rc="${?}"
 	fi
+
+	return "${rc}"
+}
+
+
+cmn::jobs::wait() {
+#
+# Waits for all child jobs running in background to finish.
+# Returns the number of failed jobs (zero means they all succeeded)
+#
+# We use `jobs -p` to get the list of child jobs running in background.
+# There might a very small risk of trying to wait for a process that would be
+# already done when calling `wait` and another one taking the same pid.
+# In this case, `wait` should fail, so it shouldn't be an issue.
+#
+
+	local rc
+	rc=0
+
+	while read -r pid; do
+    	if ! wait "${pid}"; then
+			(( rc+=1 ))
+		fi
+	done <<< "$( jobs -p )"
 
 	return "${rc}"
 }
@@ -196,7 +456,7 @@ cmn::str::join() {
 	readonly separator
 	shift
 
-	res="$( printf "${separator}%s" ${*} )"
+	res="$( printf "${separator}%s" "${@}" )"
 	# Remove leading separator:
 	res="${res:${#separator}}"
 
@@ -268,7 +528,8 @@ cmn::bp::run() {
 		return "${rc}"
 	fi
 
-	# If the repo is not reachable, fail instead of asking for credentials
+	# If the repo is not reachable, GIT_TERMINAL_PROMPT=0 allows us to fail
+	# instead of asking for credentials
 	if ! GIT_TERMINAL_PROMPT=0 \
 			git clone --quiet --depth=1 "${buildpack_url}" "${bp_dir}" \
 				2>/dev/null
@@ -299,6 +560,7 @@ cmn::bp::run() {
 readonly -f cmn::output::info
 readonly -f cmn::output::warn
 readonly -f cmn::output::err
+readonly -f cmn::output::traceback
 
 readonly -f cmn::trap::setup
 readonly -f cmn::trap::teardown
@@ -315,8 +577,12 @@ readonly -f cmn::task::start
 readonly -f cmn::task::finish
 readonly -f cmn::task::fail
 
+readonly -f cmn::cache::get
+readonly -f cmn::cache::put
+
 readonly -f cmn::file::check_checksum
 readonly -f cmn::file::download
+readonly -f cmn::file::download_and_check
 
 readonly -f cmn::str::join
 
