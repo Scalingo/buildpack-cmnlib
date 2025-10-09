6f3b42c3c124ebde427cae47363dc23906af8bca
François KUBLER <francois+github@kubler.org>
2025-10-09 15:09:13 +0200

    fix(checksum): 2 spaces are req. between filename and hash

--- cmnlib.sh
+++ cmnlib.sh
@@ -300,22 +300,22 @@ cmn::file::check_checksum() {
 
 	case "${hash_algo}" in
 		"sha1")
-			shasum --algorithm 1 --check --status <<< "${ref_hash} ${file}"
+			shasum --algorithm 1 --check --status <<< "${ref_hash}  ${file}"
 			rc="${?}"
 			;;
 
 		"sha256")
-			shasum --algorithm 256 --check --status <<< "${ref_hash} ${file}"
+			shasum --algorithm 256 --check --status <<< "${ref_hash}  ${file}"
 			rc="${?}"
 			;;
 
 		"sha512")
-			shasum --algorithm 512 --check --status <<< "${ref_hash} ${file}"
+			shasum --algorithm 512 --check --status <<< "${ref_hash}  ${file}"
 			rc="${?}"
 			;;
 
 		"md5")
-		    md5sum --check --status <<< "${ref_hash} ${file}"
+		    md5sum --check --status <<< "${ref_hash}  ${file}"
 			rc="${?}"
 			;;
 
